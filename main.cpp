d14f1ee17167a79940420728004857d95b800e55
engmhajj <144273476+engmhajj@users.noreply.github.com>
2024-09-15 21:42:25 +0300

    Start

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ enum class Ecolor
 {
     R,G
 };
+enum class Ecolor1
+{
+    R,G
+};
 int main() {
     if constexpr(Ecolor const color1= Ecolor::R; color1== Ecolor::R) {
         std::cout << "color is Res\n";
